903af569017de94053d41e65ea9d6dc3df00bf6c
Bassem <62030323+byounis@users.noreply.github.com>
2023-04-20 09:57:06 -0600

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/f/xgebrkvn",
+                url: "https://cors-anywhere.herokuapp.com/https://formspree.io/f/xgebrkvn",
                 type: "POST",
                 data: {
                     name: name,
